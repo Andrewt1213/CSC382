69414a6db1791bab671f67248d363be1941df4fd
Andrewt1213 <64708895+Andrewt1213@users.noreply.github.com>
2023-04-20 16:41:41 -0700

    cmakes

---
+++ StackFun/cmake-build-debug/CMakeFiles/StackFun.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StackFun.dir/StacksTest.cpp.obj"
+  "CMakeFiles/StackFun.dir/StacksTest.cpp.obj.d"
+  "StackFun.exe"
+  "StackFun.exe.manifest"
+  "StackFun.pdb"
+  "libStackFun.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StackFun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ StackFun/cmake-build-debug/CMakeFiles/StackFun.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for StackFun.
+# This may be replaced when dependencies are built.
