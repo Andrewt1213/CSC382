67331a9a102f65030cde13eb58a9dc95c7d2d625
Andrewt1213 <64708895+Andrewt1213@users.noreply.github.com>
2023-04-20 22:17:47 -0700

    Created New project - tic tac toe

---
+++ LambdaProject/cmake-build-debug/CMakeFiles/LambdaProject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LambdaProject.dir/LambdaFun.cpp.obj"
+  "CMakeFiles/LambdaProject.dir/LambdaFun.cpp.obj.d"
+  "CMakeFiles/LambdaProject.dir/main.cpp.obj"
+  "CMakeFiles/LambdaProject.dir/main.cpp.obj.d"
+  "LambdaProject.exe"
+  "LambdaProject.exe.manifest"
+  "LambdaProject.pdb"
+  "libLambdaProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LambdaProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LambdaProject/cmake-build-debug/CMakeFiles/LambdaProject.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for LambdaProject.
+# This may be replaced when dependencies are built.
